ac987dd4e8c0950e5a21e49e0e35806736a146b5
Adnan525 <adnaan525@gmail.com>
2021-07-16 23:45:38 +1000

    Email notification alert added. Only scheduling left

---
+++ README.md
@@ -0,0 +1,9 @@
+**Automated stock checker for xbox series x from Microsoft website**
+
+using : 
+ - Python
+ - Selenium
+ - Chrome Web Driver
+ - smtplib (**for email alert**)
+
+From main.py, change the alertStock() with desired attributes.
--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import time
-
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+from datetime import datetime
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 target = "https://www.xbox.com/en-AU/consoles/xbox-series-x#purchase"
 
-def alertStock(str):
+import smtplib
+
+def alertStock(str, retailer):
     if "OUT OF STOCK" != str:
-        print("================XBOX may be available now, check retailer's website================")
+        print(f"================XBOX may be available now, check retailer's website, retailer : {retailer}================")
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as smtpmanager:
+            smtpmanager.ehlo()
+            # encrypt
+            smtpmanager.starttls()
+            smtpmanager.ehlo()
+
+            email = "yourEmailAddress"
+            password = "password"
+
+            now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+            subject = "XBOX available now"
+            body = f"Please check the following retailer's webiste-\n Retailer: {retailer}\n time checked: {now}"
+            msg = f'From: Selenium Automation - XBOX - Adnaan\nSubject: {subject}\n\n{body}'
+
+            smtpmanager.login(email, password)
+            smtpmanager.sendmail(email, "adnaan525@gmail.com", msg)
+            smtpmanager.quit()
 
 def getRetailer(i):
     if i == 0:
@@ -42,9 +64,7 @@ buyGroup = standalonePurch.find_element_by_class_name("buy-group")
 selectRetButton = buyGroup.find_element_by_class_name("c-call-to-action")
 selectRetButton.click()
 
-time.sleep(5)
-
-
+time.sleep(3)
 #can jump straight to this instead of running the previous code blocks
 cDialog = driver.find_element_by_class_name("c-dialog")
 dialogbox = cDialog.find_element_by_class_name("dialogbox")
@@ -64,9 +84,13 @@ for retailer in hatchretailers:
     # print(retailer.find_element_by_class_name("retlogo").find_element_by_xpath("//img").get_attribute('alt'))
     price = retailer.find_element_by_class_name("retprice")
     stock = retailer.find_element_by_class_name("retstockbuy")
-    alertStock(stock.text)
+    alertStock(stock.text, retailerHardTyped)
     counter+=1
-    print("Retalier : {shop}, current price : {priceStr}, stock status : {status}".format(shop = retailerHardTyped, priceStr = price.text, status = stock.text))
+    print("Retalier : {shop}, current price : {priceStr}, stock status : {status}"
+          .format(shop = retailerHardTyped, priceStr = price.text, status = stock.text))
+
+time.sleep(3)
+driver.quit()
 
 
 
